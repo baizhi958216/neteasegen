6ac8d49ba702a9c4692955495dc85093e07ddc4a
baizhi958216 <1475289190@qq.com>
2023-06-11 19:03:22 +0800

    feat: 内置MiSans适配不同OS
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- packages/wordcloudgen.py
+++ packages/wordcloudgen.py
@@ -7,5 +7,5 @@ def pngGeneratorByList(list:list,output:str='./词云图.png'):
     for li in list:
         word.append(li.get('songName'))
         word.append(li.get('singerName'))
-    wordcloud = WordCloud(font_path='/usr/share/fonts/misans/MiSans-Normal.ttf',width=1920,height=1080).generate(' '.join(word))
+    wordcloud = WordCloud(font_path='./libs/MiSans-Normal.ttf',width=1920,height=1080).generate(' '.join(word))
     wordcloud.to_image().save(output,'png')
