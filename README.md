e2c39366d1b87b8f72e22f58237321efa08b58c6
baizhi958216 <1475289190@qq.com>
2023-06-11 22:49:59 +0800

    chore: 优化命令行参数输入
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- README.md
+++ README.md
@@ -23,17 +23,17 @@ uvicorn server:app --reload
 
 ### 生成csv文件
 ```bash
-python neteasegen.py userId=网易云用户ID gencsv 输出文件路径
+python neteasegen.py 网易云用户ID gencsv 输出文件路径
 ```
 
 ### 生成词云图
 
 ```bash
-python neteasegen.py userId=网易云用户ID genwordcloud 输出文件路径
+python neteasegen.py 网易云用户ID genwordcloud 输出文件路径
 ```
 
 ### 自定义背景词云图
 
 ```bash
-python neteasegen.py userId=网易云用户ID gencustomcloud 自定义背景词路径 输出文件路径
+python neteasegen.py 网易云用户ID gencustomcloud 自定义背景词路径 输出文件路径
 ```
--- neteasegen.py
+++ neteasegen.py
@@ -3,14 +3,14 @@ from packages.netease import getTop100
 from packages.csvgen import csvgenerator
 from packages.wordcloudgen import pngGeneratorByList,customPngGenByList
 
-userId = sys.argv[1][7:]
-command = sys.argv[2]
-
-songList = getTop100(userId)
-
-if command=='gencsv':
-    csvgenerator(songList,sys.argv[3])
-elif command=='genwordcloud':
-    pngGeneratorByList(songList,sys.argv[3])
-elif command=='gencustomcloud':
-    customPngGenByList(songList,sys.argv[3],sys.argv[4])
+try:
+    if(sys.argv[1] and sys.argv[2]):
+        songList = getTop100(sys.argv[1])
+        if sys.argv[2]=='gencsv':
+            csvgenerator(songList,sys.argv[3])
+        elif sys.argv[2]=='genwordcloud':
+            pngGeneratorByList(songList,sys.argv[3])
+        elif sys.argv[2]=='gencustomcloud':
+            customPngGenByList(songList,sys.argv[3],sys.argv[4])
+except:
+    print('参数不够\npython neteasegen.py 用户ID 生成格式 输出文件')
